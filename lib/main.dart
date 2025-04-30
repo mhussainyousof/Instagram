780607bf9bcd7de77df5c61a567df12e17b90089
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-30 22:51:36 +0330

    code cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram/features/auth/presentation/pages/auth_page.dart';
 import 'package:instagram/firebase_options.dart';
 import 'package:instagram/instagram.dart';
-import 'package:instagram/theme/light_mode.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
