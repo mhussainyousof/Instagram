f5042a7f1375ad8407cacb5bbde14b3398de7bd3
Hussain Yousof <mhussainyousof@gmail.com>
2025-05-12 15:15:36 +0330

    better performance

--- lib/instagram.dart
+++ lib/instagram.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram/colors/theme_cubit.dart';
 import 'package:instagram/features/auth/data/firebase_auth_repo.dart';
 import 'package:instagram/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:instagram/features/auth/presentation/cubit/auth_state.dart';
 import 'package:instagram/features/auth/presentation/pages/auth_page.dart';
 import 'package:instagram/features/cloudanity/data/cloudaniry_storage_repo.dart';
+import 'package:instagram/features/general/home_navigation.dart';
 import 'package:instagram/features/post/data/firebase_post_repo.dart';
 import 'package:instagram/features/post/presentation/cubit/post_cubit.dart';
 import 'package:instagram/features/profile/data/fire_base_profile_repo.dart';
 import 'package:instagram/features/profile/presentation/cubit/profile_cubit.dart';
 import 'package:instagram/features/search/data/firebase_search_repo.dart';
 import 'package:instagram/features/search/presentation/cubit/search_cubit.dart';
-import 'package:instagram/home_navigation.dart';
 import 'package:instagram/responsive/constrained_scaffold.dart';
-import 'package:instagram/theme/theme_cubit.dart';
 
 
 class MyApp extends StatelessWidget {
